3f35f10fad253187136126ab81a2707bfbea5834
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-01-10 12:44:45 -0800

    pass word and email state is working on the form

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,5 +1,26 @@
 import React from 'react';
 
-export default function AuthForm() {
-  return <div>this is the Auth form</div>;
+export default function AuthForm({ email, setEmail, password, setPassword }) {
+  return (
+    <div>
+      <form>
+        <label>E-Mail:</label>
+        <input
+          placeholder="email address"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        ></input>
+        <label>Password:</label>
+        <input
+          placeholder="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        ></input>
+      </form>
+    </div>
+  );
 }
--- src/veiws/Signup.js
+++ src/veiws/Signup.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import AuthForm from '../components/AuthForm';
+import { useState } from 'react';
 
 export default function SignUp() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div>
       <h1>Sign Up page:</h1>
-      <AuthForm></AuthForm>
+      <AuthForm
+        email={email}
+        setEmail={setEmail}
+        password={password}
+        setPassword={setPassword}
+      ></AuthForm>
     </div>
   );
 }
